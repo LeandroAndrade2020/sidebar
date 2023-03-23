a4419ae705d19ac23a18e564833a6f7414f0a9e4
Leandro Andrade <profeleandro@outlook.com>
2023-03-23 13:24:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sistema de Ponto Eletronico Digital
+
+---
+
+Desenvolvido para fazer o recolhimento da batida de horário com uma simples fotografia, sendo possível ser retirada tanto de um computador quanto de um celular.
+
+---
+
+**Sidebar** e **Breeze Starter Kit** projetados por [Ahmed Kamel](https://github.com/Kamona-WD).
+
+---
+
+## Desenvolvido e Projetado por [*Caio Willwohl Perez Ribeiro.*](https://www.linkedin.com/in/caio-willwohl-perez-ribeiro/)
+
+
+
+
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<x-guest-layout>
+    <div class="w-full h-screen
+                flex justify-center items-center">
+        <div class="w-1/2 h-1/3 text-gray-900 bg-white dark:bg-slate-400/5 dark:text-gray-200 shadow-sm">
+            <div class="p-6">
+                <h1 class="text-3xl">Welcome to {{ config('app.name') }}</h1>
+                <p class="mt-4">
+                    This is a simple Laravel app that uses Tailwind CSS.
+                </p>
+            </div>
+        </div>
+    </div>
+
+</x-guest-layout>
+
